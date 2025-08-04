be679401ca7a6b5b746040339f9bcb5a1384d1f6
Manuel Cecchin <manuelcecchinkuhn@gmail.com}>
2025-08-04 18:33:41 -0300

    segundo intento de arreglo firebase

--- app/api/usuarios/route.js
+++ app/api/usuarios/route.js
@@ -1,13 +1,13 @@
 
-import { auth } from '../../../../firebase/firebaseConfig';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+// // import { auth } from '../../../../firebase';
+// import { createUserWithEmailAndPassword } from 'firebase/auth';
 
-export async function POST(req) {
-  const { email, password } = await req.json();
-  try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    return Response.json({ uid: userCredential.user.uid });
-  } catch (err) {
-    return new Response(JSON.stringify({ error: err.message }), { status: 400 });
-  }
-}
+// export async function POST(req) {
+//   const { email, password } = await req.json();
+//   try {
+//     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+//     return Response.json({ uid: userCredential.user.uid });
+//   } catch (err) {
+//     return new Response(JSON.stringify({ error: err.message }), { status: 400 });
+//   }
+// }
